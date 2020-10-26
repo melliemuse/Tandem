0b7df30b9acc44c4b836e675b63eb9ccf79847ad
melliemuse <melliemuse@gmail.com>
2020-10-26 13:44:39 -0500

    main file setup

---
+++ src/Trivia.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+
+class Trivia extends Component {
+
+    render() {
+        return (
+            <>
+            <header className='header'> 
+            <h1>Ready to Improve your Trivia Skills?</h1>
+            <h3>Let's Get Started!</h3>
+            </header>
+            </>
+        )
+
+    }
+
+}
+
+export default Trivia
